195750c9afaa2a7527a0e7d4c0bc510d3a96d47f
wafeliciano20 <firstwillow2@gmail.com>
2021-06-06 18:56:37 -0400

    edited the number of videos from the api and change the page title

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -5,7 +5,7 @@ export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
-        maxResults: 25,
+        maxResults: 5,
         type: 'video',
         key: process.env.REACT_APP_API_KEY
     }
